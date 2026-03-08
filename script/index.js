24d1b6d0ec0d8ca6bff8cbbe81b1d80a3b1a6c38
Mehedi <mehedi.interface@gmail.com>
2026-03-08 20:14:30 +0600

    29-4 Load Words by Level and Display Cards

--- script/index.js
+++ script/index.js
@@ -4,6 +4,30 @@ const loadLesson = () => {
     .then((json) => displayLesson(json.data)); 
 }; 
 
+const loadLevelWord = (id) => {
+    // console.log(id); 
+    const url = `https://openapi.programming-hero.com/api/level/${id}`; 
+    fetch(url) 
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data))
+}; 
+
+const displayLevelWord = (words) => {
+    // console.log(words); 
+    const wordContainer = document.getElementById("word-container"); 
+    wordContainer.innerHTML = ""; 
+
+    words.forEach((word) => {
+        console.log(word); 
+        const card = document.createElement("div"); 
+        card.innerHTML = `
+        <p>Cat</p>
+        `; 
+        wordContainer.append(card); 
+    })
+
+}
+
 const displayLesson = (lessons) => {
     // console.log(lessons)
     // 1. get the container & empty
@@ -13,10 +37,10 @@ const displayLesson = (lessons) => {
     // 2. get intuo every lessons 
     for (let lesson of lessons) {
         // 3. create element
-        console.log(lesson)
+        console.log(lesson); 
         const btnDiv = document.createElement("div"); 
         btnDiv.innerHTML = `
-        <button href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `
         // 4. append into container
         levelContainer.append(btnDiv)
