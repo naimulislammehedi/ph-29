080328f07d7e0ce7a07cb7a3a5b862cc067b9bc2
Mehedi <mehedi.interface@gmail.com>
2026-03-08 16:30:00 +0600

    29-3 Load and Display All Lessons with API as Figma

--- script/index.js
+++ script/index.js
@@ -1,31 +1,29 @@
-const loadLessons = () => {
-    fetch("https://openapi.programming-hero.com/api/levels/all")
-        .then((res) => res.json())
-        .then((json) => displayLesson(json.data));
-};
+const loadLesson = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response 
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data)); 
+}; 
 
 const displayLesson = (lessons) => {
-    // 1. get the container and empty
-    const levelContainer = document.getElementById("level-container");
+    // console.log(lessons)
+    // 1. get the container & empty
+    const levelContainer = document.getElementById("level-container"); 
+    levelContainer.innerHTML = ""; 
 
-    levelContainer.innerHTML = "";
-
-    // 2. get into every lessons 
+    // 2. get intuo every lessons 
     for (let lesson of lessons) {
         // 3. create element
-        console.log(lesson); 
-        const btnDiv = document.createElement("div");
+        console.log(lesson)
+        const btnDiv = document.createElement("div"); 
         btnDiv.innerHTML = `
-            <button href="#" class="btn btn-outline btn-primary">
-            <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
-            </button>
-        `; 
-
+        <button href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        `
         // 4. append into container
-        levelContainer.append(btnDiv);
+        levelContainer.append(btnDiv)
+
     }
+    
+    
+}
 
-
-};
-
-loadLessons(); 
+loadLesson(); 
